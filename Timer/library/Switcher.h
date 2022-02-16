67c60625be55e85445443f9ee56de415c7a7528b
Berto 'd Sera <berto.d.sera@gmail.com>
2022-02-16 16:06:31 +0100

    licensing info added

--- Timer/library/Switcher.h
+++ Timer/library/Switcher.h
@@ -1,4 +1,15 @@
 /*
+ * =======================================================================================================================
+ * This file is part of Darkroom.
+ * Darkroom is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+ * Darkroom is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with Darkroom.
+ * If not, see <https://www.gnu.org/licenses/>.
+ * =======================================================================================================================
+ *
  * Switcher.h
  *
  * Manage coupled PushButton and LedSignal Objects as a single entity
